0b3f60e5099e316ad880b6fadb4489a9251e58be
yangbin <yangbin@star-net.cn>
2019-04-03 17:29:45 +0800

    提交 柱放置边框识别 插件 SelectColumnFrame

---
+++ LibreCAD-2.2.0-rc1/plugins/SelectColumnFrame/SelectColumnFrame.cpp
@@ -0,0 +1,518 @@
+﻿/*****************************************************************************/
+/*  list.cpp - List selected entities                                        */
+/*                                                                           */
+/*  Copyright (C) 2011 Rallaz, rallazz@gmail.com                             */
+/*                                                                           */
+/*  This library is free software, licensed under the terms of the GNU       */
+/*  General Public License as published by the Free Software Foundation,     */
+/*  either version 2 of the License, or (at your option) any later version.  */
+/*  You should have received a copy of the GNU General Public License        */
+/*  along with this program.  If not, see <http://www.gnu.org/licenses/>.    */
+/*****************************************************************************/
+
+
+#include <QTextEdit>
+//#include <QColor>
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
+#include <cmath>
+#include "SelectColumnFrame.h"
+
+// yangbin
+#ifndef M_PI
+#define M_PI       3.141592653589793238462643
+#endif
+#ifndef M_PI_2
+#define M_PI_2       1.57079632679489661923
+#endif
+#define M_PIx2      6.283185307179586 // 2*PI
+
+QString LC_List::name() const
+ {
+     return (tr("handle column table"));
+ }
+
+PluginCapabilities LC_List::getCapabilities() const
+{
+    PluginCapabilities pluginCapabilities;
+    pluginCapabilities.menuEntryPoints
+            << PluginMenuLocation("plugins_menu", tr("select column frame"));
+    return pluginCapabilities;
+}
+
+
+bool LC_List::sign(const QPointF& v1, const QPointF& v2, const QPointF& v3) {
+	double res = (v1.x() - v3.x())*(v2.y() - v3.y()) - (v2.x() - v3.x())*(v1.y() - v3.y());
+	return (res >= 0.0);
+}
+
+/**
+* 计算两个向量的夹角 (in rad).
+*/
+double angle(QPointF a, QPointF b) {
+	// 先将向量标准化
+	double al = sqrt(a.x() * a.x() + a.y() * a.y());
+	double bl = sqrt(b.x() * b.x() + b.y() * b.y());
+	double ab = a.x() * b.x() + a.y() * b.y();
+	double cos = ab / (al * bl);
+	if (cos > 1.0) cos = 1.0;
+	if (cos < -1.0) cos = -1.0;
+	return acos(cos);
+	/*a = a / al; b = b / bl;
+	double x = a.x() - b.x();
+	double y = a.y() - b.y();
+	return fmod(M_PI + remainder(atan2(y, x) - M_PI, m_piX2), m_piX2);*/
+}
+
+/**
+* 计算三角形的面积，利用叉乘.
+*/
+double triangleArea(const QPointF& v1, const QPointF& v2, const QPointF& v3) {
+	QPointF e1 = v1 - v3;
+	QPointF e2 = v2 - v3;
+	return  abs(e1.x() * e2.y() - e2.x() * e1.y()) / 2;
+}
+
+/**
+* 计算点到直线的距离.
+*/
+double pointToLine(const QPointF& pt, const QPointF& v1, const QPointF& v2) {
+
+	double a, b, c;		// 直线一般式方程的系数
+	a = v2.y() - v1.y();
+	b = v1.x() - v2.x();
+	c = v2.x() * v1.y() - v1.x() * v2.y();
+
+	return abs(a * pt.x() + b * pt.y() + c) / sqrt(a * a + b * b);
+}
+
+bool isInsideTriangle(const QPointF& pt, const QPointF& v1, const QPointF& v2, const QPointF& v3) {
+	double s1 = triangleArea(pt, v1, v2);
+	double s2 = triangleArea(pt, v2, v3);
+	double s3 = triangleArea(pt, v3, v1);
+	double s4 = triangleArea(v1, v2, v3);
+	double ss = abs(s4 - s1 - s2 - s3);
+	if (ss < 1.0e-7) return true;
+	return false;
+}
+
+/* 0 在polyline 外部， 1 在polyline 边线或者顶点上， 2 在polyline 内部 */
+int isInsidePolyline(const QPointF& pt, std::vector<QPointF>& polyline) {
+	// 1，判断是否在 Polyline 外围的矩形外
+	QPointF minPt, maxPt;
+	for (int i = 0; i < polyline.size(); i++) {
+		if (polyline[i].x() < minPt.x()) minPt.setX(polyline[i].x());
+		if (polyline[i].y() < minPt.y()) minPt.setY(polyline[i].y());
+
+		if (polyline[i].x() > maxPt.x()) maxPt.setX(polyline[i].x());
+		if (polyline[i].y() > maxPt.y()) maxPt.setY(polyline[i].y());
+	}
+	if (pt.x() > maxPt.x() || pt.x() < minPt.x() || pt.y() > maxPt.y() || pt.y() < minPt.y())
+		return 0;
+
+	// 2，判断是否在 Polyline 的顶点上
+	for (int i = 0; i < polyline.size(); i++) {
+		if (polyline[i].x() == pt.x() && polyline[i].y() == pt.y())
+			return 1;
+	}
+
+	// 3，判断是否在 Polyline 的边线上
+	for (int i = 0, j = polyline.size() - 1; i < polyline.size(); j = i++) {
+		QPointF e1, e2, p1, p2;
+		p1 = polyline[i];
+		p2 = polyline[j];
+		e1 = pt - p1;
+		e2 = pt - p2;
+		if (fabs(e1.x() * e2.y() - e2.x() * e1.y()) < eps
+			&& min(p1.x(), p2.x()) - eps <= pt.x()
+			&& max(p1.x(), p2.x()) + eps >= pt.x()
+			&& min(p1.y(), p2.y()) - eps <= pt.y()
+			&& max(p1.y(), p2.y()) + eps >= pt.y())
+			return 1;
+	}
+
+	// 4，判断是否在 Polyline 外部 或 内部 （射线法）
+	bool c = false;
+	for (int i = 0, j = polyline.size() - 1; i < polyline.size(); j = i++) {
+		QPointF p1, p2;
+		p1 = polyline[i];
+		p2 = polyline[j];
+		// 以 pt 为起点向右水平射出
+		if (((p1.y() > pt.y()) != (p2.y() > pt.y())) &&
+			(pt.x() < (p2.x() - p1.x()) * (pt.y() - p1.y()) / (p2.y() - p1.y()) + p1.x()))
+			c = !c;
+	}
+	if (c) return 2;
+	return 0;
+}
+
+QPointF leftUpCorner(std::vector<QPointF>& polyline) {
+	QPointF ld;
+	for (int i = 0; i < polyline.size(); i++) {
+		if (i == 0) ld = polyline[i];
+		else {
+			if (ld.y() + 1.0 < polyline[i].y()) {
+				ld = polyline[i];
+			}
+			else if (ld.y() - 1.0 <= polyline[i].y()) {
+				if (ld.x() > polyline[i].x())
+					ld = polyline[i];
+			}
+		}
+	}
+	return ld;
+}
+
+/* 计算点到 Polyline 的最近距离, 仅判断线段端点，有可能垂足在线段中间 */
+double pointToPolyline(const QPointF& pt, std::vector<QPointF>& polyline) {
+	double dist = 1.0e+20;
+	for (int i = 0; i < polyline.size(); i++) {
+		QPointF p1;
+		p1 = polyline[i];
+		p1 = pt - p1;
+		double dist0 = sqrt(p1.x() * p1.x() + p1.y() * p1.y());
+
+		if (dist0 < dist) dist = dist0;
+	}
+	return dist;
+}
+
+/* 计算点垂直向上到 Polyline 是否有交点  */
+int pointCrossPolyline(const QPointF& pt, std::vector<QPointF>& polyline) {
+	int nCross = 0;
+	for (int i = 0, j = polyline.size() - 1; i < polyline.size(); j = i++) {
+		QPointF p1, p2;
+		p1 = polyline[i];
+		p2 = polyline[j];
+		// 以 pt 为起点向上垂直射出
+		if (((p1.x() > pt.x()) != (p2.x() > pt.x())) &&
+			(pt.y() < (p2.y() - p1.y()) * (pt.x() - p1.x()) / (p2.x() - p1.x()) + p1.y()))
+			nCross++;
+	}
+	return nCross;
+}
+
+
+/* 第一遍，过滤 柱放置边框 */
+void filterData1(Plug_Entity *ent, std::vector<StripData>& strips) {
+	if (NULL == ent)
+		return ;
+
+	QHash<int, QVariant> data;
+	//common entity data
+	ent->getData(&data);
+
+	//specific entity data
+	int et = data.value(DPI::ETYPE).toInt();
+	switch (et) {
+	case DPI::POLYLINE: {
+		/*QString strLayer = data.value(DPI::LAYER).toString();
+		QString strPattern = QString::fromLocal8Bit("柱大样外框");*/
+		if (/*strLayer.indexOf(strPattern) >=0*/ true) {
+			StripData strip;
+			strip.strLayer = data.value(DPI::LAYER).toString();
+			strip.strColor = ent->intColor2str(data.value(DPI::COLOR).toInt());
+			strip.closed = data.value(DPI::CLOSEPOLY).toInt();
+			strip.ent = ent;
+			
+			QList<Plug_VertexData> vl;
+			ent->getPolylineData(&vl);
+			int iVertices = vl.size();
+			for (int i = 0; i < iVertices; ++i) {
+				strip.vertexs.push_back(vl.at(i).point);
+			}
+
+			if (iVertices >= 3) {
+				strips.push_back(strip);
+			}
+		}
+		break; }
+	default:
+		break;
+	}
+
+}
+
+
+// 第二遍，匹配 柱附近的标注引出线
+void filterData2(Plug_Entity *ent, std::vector<StripData>& strips, std::vector<LineData>& lines) {
+	if (NULL == ent)
+		return;
+
+	QPointF ptA, ptB;
+	bool bOutside;
+	QString textContent;
+	QHash<int, QVariant> data;
+	//common entity data
+	ent->getData(&data);
+
+	//specific entity data
+	int et = data.value(DPI::ETYPE).toInt();
+	int i;
+	switch (et) {
+	
+	case DPI::LINE:
+		bOutside = true;
+		LineData line;
+		line.from.setX(data.value(DPI::STARTX).toDouble());
+		line.from.setY(data.value(DPI::STARTY).toDouble());
+		line.to.setX(data.value(DPI::ENDX).toDouble());
+		line.to.setY(data.value(DPI::ENDY).toDouble());
+		line.ent = ent;
+		for (i = 0; i < strips.size(); i++) {
+			if (isInsidePolyline(line.from, strips[i].vertexs) && !isInsidePolyline(line.to, strips[i].vertexs)) {
+				strips[i].lines.push_back(line);
+				break;
+			}
+			else if (!isInsidePolyline(line.from, strips[i].vertexs) && isInsidePolyline(line.to, strips[i].vertexs)) {
+				QPointF tmp = line.from;
+				line.from = line.to;
+				line.to = tmp;
+				strips[i].lines.push_back(line);
+				break;
+			}
+		}
+		if (i >= strips.size()) {
+			lines.push_back(line);
+		}
+		break;
+	default:
+		break;
+	}
+
+}
+
+// 分发标注引用线
+void dispatchLines(std::vector<StripData>& strips, std::vector<LineData>& lines) {
+	bool bRunning = true;
+	while (bRunning) {
+		bRunning = false;
+		std::vector<LineData>::iterator it;
+		for (it =lines.begin(); it != lines.end(); it++) {
+			// it 表示待分发的 line 
+			int i;
+			for (i = 0; i < strips.size(); i++) {
+				if (strips[i].lines.size() > 0) {
+					// 取出该柱子的最后一条标注引出线
+					LineData line = strips[i].lines[strips[i].lines.size() - 1];
+					if (line.to == it->from) {
+						strips[i].lines.push_back(*it);
+						break;
+					}
+					else if (line.to == it->to) {
+						QPointF tmp = it->from;
+						it->from = it->to;
+						it->to = tmp;
+						strips[i].lines.push_back(*it);
+						break;
+					}
+				}
+			}
+			if (i < strips.size()) {
+				// 对于已经分发成功的 line ，从待分发队列中删除
+				lines.erase(it);
+				bRunning = true;
+				break;
+			}
+		}
+	}
+}
+
+
+// 第三遍，匹配 柱文本标注 到 柱放置边框
+void filterData3(Plug_Entity *ent, std::vector<StripData>& strips) {
+	if (NULL == ent)
+		return;
+
+	QPointF ptA, ptB;
+	bool bOutside;
+	QString textContent;
+	QHash<int, QVariant> data;
+	//common entity data
+	ent->getData(&data);
+
+	//specific entity data
+	int et = data.value(DPI::ETYPE).toInt();
+	switch (et) {
+
+	case DPI::MTEXT:
+	case DPI::TEXT:
+		textContent = data.value(DPI::TEXTCONTENT).toString();
+		bOutside = true;
+		ptA = ent->getMaxOfBorder();
+		ptB = ent->getMinOfBorder();
+		for (int i = 0; i < strips.size(); i++) {
+			if (isInsidePolyline(ptA, strips[i].vertexs) || isInsidePolyline(ptB, strips[i].vertexs)) {
+				bOutside = false;
+				break;
+			}
+		}
+		if (bOutside) {
+			// 寻找 mid 点正上方最近的 柱大样
+			QPointF mid = (ptA + ptB) / 2;
+			int closestColumn = -1;
+			double dist = 1.0e+20;
+			for (int i = 0; i < strips.size(); i++) {
+				if (pointCrossPolyline(ptA, strips[i].vertexs)
+					|| pointCrossPolyline(ptB, strips[i].vertexs)
+					|| pointCrossPolyline(mid, strips[i].vertexs)) {
+					double dist1 = pointToPolyline(mid, strips[i].vertexs);
+					if (closestColumn < 0) {
+						closestColumn = i;
+						dist = dist1;
+					}
+					else if (dist1 < dist) {
+						closestColumn = i;
+						dist = dist1;
+					}
+				}
+			}
+
+			if (closestColumn >= 0) {
+				if (textContent.indexOf(QRegExp("^[0-9]+C")) >= 0) {
+					if (strips[closestColumn].steelLongitudinal.isEmpty()) {
+						strips[closestColumn].steelLongitudinal = textContent;
+						// 删除该文本，随后生成截面标注
+						entites.push_back(ent);
+					}
+
+				}
+				else if (textContent.indexOf(QRegExp("^[ABCDE][0-9]+")) >= 0) {
+					if (strips[closestColumn].steelHooping.isEmpty()) {
+						strips[closestColumn].steelHooping = textContent;
+						// 删除该文本，随后生成截面标注
+						entites.push_back(ent);
+					}
+				}
+				else if (textContent.indexOf(QRegExp("^[A-Z0-9a-z]+")) >= 0) {
+					if (strips[closestColumn].name.isEmpty()) {
+						strips[closestColumn].name = textContent;
+						// 删除该文本，随后生成截面标注
+						entites.push_back(ent);
+					}
+				}
+				else {
+					entites.push_back(ent);
+				}
+			}
+			else {
+				// 无关的文本删除
+				entites.push_back(ent);
+			}
+		}
+		break;
+	default:
+		break;
+	}
+
+}
+
+QString LC_List::getStrData(StripData strip) {
+    
+	QString strData(""), strCommon("  %1: %2\n");
+    
+	strData = strCommon.arg(strip.vertexs[0].x()).arg(strip.vertexs[0].y());
+	if (!strip.closed) {
+		strData.append(strCommon.arg(tr("closed")).arg("no"));
+	}
+    strData.append(strCommon.arg(tr("columnName")).arg(strip.name));
+	strData.append(strCommon.arg(tr("steelLongitudinal")).arg(strip.steelLongitudinal));
+	strData.append(strCommon.arg(tr("steelHooping")).arg(strip.steelHooping));
+
+    return strData;
+}
+
+
+void LC_List::execComm(Document_Interface *doc,
+	QWidget *parent, QString cmd)
+{
+	Q_UNUSED(parent);
+	Q_UNUSED(cmd);
+	d = doc;
+	QList<Plug_Entity *> obj;
+	std::vector<Plug_Entity *> entites;
+	bool yes = doc->getSelect(&obj);
+	if (!yes || obj.isEmpty()) return;
+
+	//	柱大样外边框线
+	std::vector<StripData>  strips;
+	for (int i = 0; i < obj.size(); ++i) {
+		filterData1(obj.at(i), strips);
+	}
+	if (strips.size() > 0) {
+		for (int i = 0; i < obj.size(); ++i) {
+			filterData2(obj.at(i), strips, entites);
+		}
+	}
+
+	QString text;
+	for (int i = 0; i < strips.size(); ++i) {
+		text.append(QString("%1 %2: ").arg(tr("n")).arg(i + 1));
+		text.append(getStrData(strips[i]));
+		text.append("\n");
+	}
+	lc_Listdlg dlg(parent);
+	dlg.setText(text);
+	//dlg.exec();
+	if (dlg.exec()) {
+		// 如果是 close 按钮，则删除已正确识别的板带 
+		for (int k = 0; k < entites.size(); k++) {
+			doc->removeEntity(entites[k]);
+		}
+		
+		for (int i = 0; i < strips.size(); ++i) {
+			// 对于未封闭的柱大样边线，要封闭
+			if (!strips[i].closed) {
+				QHash<int, QVariant> hash;
+				hash.insert(DPI::CLOSEPOLY, true);
+				strips[i].ent->updateData(&hash);
+			}
+
+			// 生成 柱的截面标注
+			QPointF start = leftUpCorner(strips[i].vertexs) + QPointF(200,-5);
+			QPointF end = start + QPointF(0, 2300);
+			doc->addLine(&start, &end);
+			end = end + QPointF(50, -300);
+			doc->addText(strips[i].name, "standard", &end, 280, 0, DPI::HAlignLeft, DPI::VAlignMiddle);
+			end = end + QPointF(0, -300);
+			doc->addText(strips[i].steelLongitudinal, "standard", &end, 280, 0, DPI::HAlignLeft, DPI::VAlignMiddle);
+			end = end + QPointF(0, -300);
+			doc->addText(strips[i].steelHooping, "standard", &end, 280, 0, DPI::HAlignLeft, DPI::VAlignMiddle);
+		}
+	}
+
+	while (!obj.isEmpty())
+		delete obj.takeFirst();
+}
+
+double LC_List::polylineRadius( const Plug_VertexData& ptA, const Plug_VertexData& ptB)
+{
+    double dChord = sqrt( pow(ptA.point.x() - ptB.point.x(), 2) + pow(ptA.point.y() - ptB.point.y(), 2));
+
+    return fabs( 0.5 * dChord / sin( 2.0 * atan(ptA.bulge)));
+}
+
+/*****************************/
+lc_Listdlg::lc_Listdlg(QWidget *parent) :  QDialog(parent)
+{
+    setWindowTitle(tr("List strip entities"));
+//    QTextEdit *edit= new QTextEdit(this);
+    edit.setReadOnly (true);
+    edit.setAcceptRichText ( false );
+    QDialogButtonBox* bb = new QDialogButtonBox( QDialogButtonBox::Close, Qt::Horizontal, this );
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addWidget(&edit);
+    mainLayout->addWidget(bb);
+    this->setLayout(mainLayout);
+    this->resize ( 650, 350 );
+
+    connect(bb, SIGNAL(rejected()), this, SLOT(accept()));
+}
+
+void lc_Listdlg::setText(QString text)
+{
+    edit.setText(text);
+}
+lc_Listdlg::~lc_Listdlg()
+{
+}
---
+++ LibreCAD-2.2.0-rc1/plugins/SelectColumnFrame/SelectColumnFrame.h
@@ -0,0 +1,121 @@
+/*****************************************************************************/
+/*  list.h - List selected entities                                          */
+/*                                                                           */
+/*  Copyright (C) 2011 Rallaz, rallazz@gmail.com                             */
+/*                                                                           */
+/*  This library is free software, licensed under the terms of the GNU       */
+/*  General Public License as published by the Free Software Foundation,     */
+/*  either version 2 of the License, or (at your option) any later version.  */
+/*  You should have received a copy of the GNU General Public License        */
+/*  along with this program.  If not, see <http://www.gnu.org/licenses/>.    */
+/*****************************************************************************/
+
+#ifndef SelectColumnFrame_H
+#define SelectColumnFrame_H
+
+#include <QDialog>
+#include "qc_plugininterface.h"
+#include "document_interface.h"
+#include <QTextEdit>
+
+#define M_PI       3.14159265358979323846   // pi
+
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+
+const double eps = 1.0e-8;
+
+namespace {
+	constexpr double m_piX2 = M_PI * 2; //2*PI
+}
+
+
+typedef struct _LineData {
+	/**
+	 * Default constructor. Leaves the data object uninitialized.
+	 */
+	_LineData() = default;
+
+	QPointF from, to;
+	Plug_Entity * ent;
+} LineData;
+
+typedef struct _StripData {
+	/**
+	 * Default constructor. Leaves the data object uninitialized.
+	 */
+	_StripData() = default;
+	   
+	int closed;
+	/** Control points of the spline. */
+	// 柱边线控制点
+	std::vector<QPointF> vertexs;
+
+	// 标注引出线 (一般是两条)
+	std::vector<LineData> lines;
+
+	QString name;					// 柱名称
+
+	QString strLayer, strColor;
+	Plug_Entity * ent;
+} StripData;
+
+
+//class QTextEdit;
+class Plug_Entity;
+
+class LC_List : public QObject, QC_PluginInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(QC_PluginInterface)
+    Q_PLUGIN_METADATA(IID LC_DocumentInterface_iid FILE  "SelectColumnFrame.json")
+
+ public:
+    virtual PluginCapabilities getCapabilities() const Q_DECL_OVERRIDE;
+    virtual QString name() const Q_DECL_OVERRIDE;
+    virtual void execComm(Document_Interface *doc,
+                          QWidget *parent, QString cmd) Q_DECL_OVERRIDE;
+
+private:
+	bool sign(const QPointF& v1, const QPointF& v2, const QPointF& v3);
+	// 第一遍，过滤 柱大样边线
+	void filterData1(Plug_Entity *ent, std::vector<StripData>& strips);
+	// 第二遍，过滤 其它钢筋线，以及箍筋和纵筋的标识
+	void filterData2(Plug_Entity *ent, std::vector<StripData>& strips, std::vector<Plug_Entity *>& entites);
+	
+    QString getStrData(StripData strip);
+    double polylineRadius( const Plug_VertexData& ptA, const Plug_VertexData& ptB);
+    Document_Interface *d;
+};
+
+class lc_Listdlg : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit lc_Listdlg(QWidget *parent = 0);
+    ~lc_Listdlg();
+    void setText(QString text);
+
+public slots:
+//    void processAction(QStringList *commandList);
+//    void processAction(Document_Interface *doc);
+//    void checkAccept();
+
+protected:
+//    void closeEvent(QCloseEvent *event);
+
+private:
+//    void readSettings();
+//    void writeSettings();
+//    bool failGUI(QString *msg);
+
+private:
+//    QString errmsg;
+//    QLineEdit *startxedit;
+//    QLineEdit *startyedit;
+//    QLineEdit *endxedit;
+    QTextEdit edit;
+};
+
+#endif // LIST_H
