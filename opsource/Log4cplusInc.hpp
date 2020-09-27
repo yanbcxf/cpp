6d65142d7edef5125ea567fc58d5be6925d5037a
yangbin <yangbin@star-net.cn>
2020-09-27 15:46:17 +0800

    FEATURE 补充提交 Log4cplusInc.hpp 文件

---
+++ opsource/Log4cplusInc.hpp
@@ -0,0 +1,20 @@
+#ifndef _LOG4CPLUSINC_HPP
+#define _LOG4CPLUSINC_HPP
+
+
+#include <log4cplus/logger.h>
+#include <log4cplus/configurator.h>
+#include <log4cplus/helpers/loglog.h>
+#include <log4cplus/helpers/stringhelper.h>
+
+#include "log4cplus/fileappender.h"
+#include "log4cplus/win32debugappender.h" 
+#include "log4cplus/consoleappender.h"
+#include "log4cplus/layout.h" 
+
+using namespace log4cplus;
+using namespace log4cplus::helpers;
+  
+
+
+#endif
